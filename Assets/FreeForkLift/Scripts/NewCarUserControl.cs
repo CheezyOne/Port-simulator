e914b4f00bd80799e4f5476706acb0324fdcbd41
CheezyOne <Lavrovvova0103@gmail.com>
2025-03-13 15:56:49 +0300

    Cleared code, changed questions

--- Assets/FreeForkLift/Scripts/NewCarUserControl.cs
+++ Assets/FreeForkLift/Scripts/NewCarUserControl.cs
@@ -1,31 +1,16 @@
-using System;
 using UnityEngine;
 
-    [RequireComponent(typeof (NewCarController))]
+[RequireComponent(typeof(NewCarController))]
 public class NewCarUserControl : MonoBehaviour
+{
+    [SerializeField] private NewCarController _carController; // the car controller we want to use
+
+    private void FixedUpdate()
     {
-        private NewCarController m_Car; // the car controller we want to use
-
-        private void Awake()
-        {
-            // get the car controller
-            m_Car = GetComponent<NewCarController>();
-        }
-
-
-        private void FixedUpdate()
-        {
-            // pass the input to the car!
-            float h = Input.GetAxis("Horizontal");
-            float v = Input.GetAxis("Vertical");
-
-#if !MOBILE_INPUT
-            float handbrake = Input.GetAxis("Jump");
-            m_Car.Move(h, v, v, handbrake);
-            
-#else
-            m_Car.Move(h, v, v, 0f);
-#endif
-        }
+        // pass the input to the car!
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float handbrake = Input.GetAxis("Jump");
+        _carController.Move(h, v, v, handbrake);
     }
-
+}
